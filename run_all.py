c18275f47ebb125c44b8569b3b125fa4319757b4
jitesh-metrisum <jitesh@mindassets.in>
2025-09-24 15:34:43 +0530

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,8 +1,10 @@
 import subprocess
 
-scripts = [ "sales_orders_report.py"]
+scripts = [
+    "sales_orders_report.py",
+    # Add other report scripts here
+]
 
 for s in scripts:
     print(f"Running {s}...")
     subprocess.run(["python", s], check=True)
-    print(f"{s} finished.")
